abf382feaa01dc552b550d7a49faa9ef5a2a69f1
haiun <gewdox@naver.com>
2021-03-25 12:20:12 +0900

    Player 클래스 오탈자 수정.

--- GrpcService1/Shared/Player.cs
+++ GrpcService1/Shared/Player.cs
@@ -7,8 +7,8 @@ namespace GrpcService1.Shared
     public class Player
     {
         [Key(0)]
-        public string Name { get; init; }
-        [Key((1))]
+        public string Name { get; set; }
+        [Key(1)]
         public Vector3 Position { get; set; }
         [Key(2)]
         public Quaternion Rotation { get; set; }
